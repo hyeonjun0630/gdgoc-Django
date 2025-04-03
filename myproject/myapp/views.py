70b1b67b9a3f8b972cf4508feec37125dd9f2d1d
haru0630 <hyeonjun@inha.edu>
2025-04-03 11:20:14 +0000

    4월3일 API만들기 실습

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class PostRetriveUpdate(APIView):
+
+    def get(self, request):
+        return Response({"hello":"world"}, status=200)
+
+    def post(self, request):
+        title = request.data.get("title")
+        if title:
+            return Response({"title":title}, status=200)
+        return Response({}, status=400)
